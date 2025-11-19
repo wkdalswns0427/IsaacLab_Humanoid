eeb9c8daaf949b884160a6b39300e690bb7835cc
mchang344 <minjunchang@gatech.edu>
2025-11-19 17:52:38 -0500

    building on h1 vanila

--- h1_dext_tasks/source/h1_dext_tasks/h1_dext_tasks/tasks/manager_based/h1_dext_tasks/__init__.py
+++ h1_dext_tasks/source/h1_dext_tasks/h1_dext_tasks/tasks/manager_based/h1_dext_tasks/__init__.py
@@ -22,3 +22,14 @@ gym.register(
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
     },
 )
+
+gym.register(
+    id="Template-H1-Dext-Tasks-Rough-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.h1_dext_tasks_env_rough_cfg:H1DextTasksEnvCfg",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
+    },
+)
